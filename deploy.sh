24a494ee76a3a651d4f716a81d5580b0cc941fca
AlexeyNavalniyPrinted <117517842+AlexeyNavalniyPrinted@users.noreply.github.com>
2024-08-11 21:44:23 +0300

    Make it simplier + small fixes

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,6 @@ kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/late
 # kubectl edit deploy metrics-server -n kube-system
 # and set the arg --kubelet-insecure-tls on the metrics server container
 
-helm repo add istio https://istio-release.storage.googleapis.com/charts
-helm repo update istio
-
-kubectl create namespace istio-system
-
-helm install istio istio/base --namespace istio-system --wait
-helm install istiod istio/istiod -n istio-system --wait
-helm install istio-ingress istio/gateway -n istio-system --wait
-
-kubectl label namespace default istio-injection=enabled
-kubectl apply -f ./manifests/istio/
 kubectl apply -f ./manifests/scaling/HPAs.yaml
 kubectl apply -f ./manifests/
 
